59bc4a430eb22f7c27f33278a4bbff4f00202028
gpbacay <111699545+gpbacay@users.noreply.github.com>
2022-09-14 13:40:47 +0800

    ....

--- VIRTUAL-ASSISTANT-AI-PROJECT/Virtual_Assistant_AI_Project/KHRYZEN.py
+++ VIRTUAL-ASSISTANT-AI-PROJECT/Virtual_Assistant_AI_Project/KHRYZEN.py
@@ -1,3 +1,4 @@
+#Import Libraries
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
@@ -5,10 +6,13 @@ import datetime
 import wikipedia
 import pyjokes
 
+#Declare Variables
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+
+#Voice Activation
+engine.setProperty('voice', voices[2].id)
 
 
 def talk(text):
@@ -19,42 +23,44 @@ def talk(text):
 def take_command():
     try:
         with sr.Microphone() as source:
-            print('listening...')
+            print('Listening...')
+            talk('Im listening')
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
-            if 'alexa' in command:
-                command = command.replace('alexa', '')
+            if 'khryzen' in command:
+                command = command.replace('khryzen', '')
                 print(command)
     except:
         pass
     return command
 
-
-def run_alexa():
+#Input Commands
+def run_khryzen():
     command = take_command()
     print(command)
     if 'play' in command:
         song = command.replace('play', '')
-        talk('playing ' + song)
+        talk('playing' + song)
         pywhatkit.playonyt(song)
     elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
+        time = datetime.datime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Current time is' + time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
-    elif 'joke' in command:
+    elif 'Who created you?' in command:
+        talk('Gianne Bacay was my creator')
+    elif 'tell me a joke' in command:
         talk(pyjokes.get_joke())
     else:
-        talk('Please say the command again.')
+        talk('pardon me, please say the command again.')
 
-
+#While Loop
 while True:
-    run_alexa()
+    run_khryzen()
+
+
